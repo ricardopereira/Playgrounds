3c75000b022ebc8c5c120b5d20050e28f1ef848d
Ricardo Pereira <m@ricardopereira.eu>
2016-08-02 09:34:00 +0100

    Update sticky cell header collection

--- StickyCollectionLayout.playground/Contents.swift
+++ StickyCollectionLayout.playground/Contents.swift
@@ -54,13 +54,13 @@ class StickyCellCollectionViewLayout: UICollectionViewFlowLayout {
     func setActiveAttributes(attributes: UICollectionViewLayoutAttributes) {
         guard let collectionView = collectionView else { return }
         let contentOffset = collectionView.contentOffset.y
-        if CGRectGetMinY(attributes.frame) < contentOffset {
+        if attributes.frame.minY < contentOffset {
             // Attributes want to be above the visible rect, so let's stick it to the top.
             attributes.frame.origin.y = contentOffset
         }
-        else if CGRectGetMaxY(attributes.frame) > CGRectGetHeight(collectionView.frame) + contentOffset {
+        else if attributes.frame.maxY > collectionView.frame.height + contentOffset {
             // Attributes want to be below the visible rect, so let's stick it to the bottom (height + contentOffset - height of attributes)
-            attributes.frame.origin.y = CGRectGetHeight(collectionView.frame) + contentOffset - CGRectGetHeight(attributes.frame)
+            attributes.frame.origin.y = collectionView.frame.height + contentOffset - attributes.frame.height
         }
         attributes.zIndex = 1
     }
